5e48e90e9b511342c4810cb203030e72fa980d57
Zouhair <darkzouhair@gmail.com>
2024-07-25 07:53:17 -0700

    classes

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,19 @@
+import Car from './10-car';
+
+class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  cloneCar() {
+    const newCar = new Car(this._brand, this._motor, this._color);
+    return newCar;
+  }
+}
+
+export default EVCar;
